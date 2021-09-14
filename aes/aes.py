150e23e40d948da64ab043f4abccbaa2120fdf6d
Fernando Maluenda <fmaluendap@gmail.com>
2021-09-13 19:43:41 -0600

    Add note to grader

--- aes/aes.py
+++ aes/aes.py
@@ -4,6 +4,10 @@ from formatter import Formatter as ftr
 
 
 def aes():
+    """
+    Note for grader: You only need to change the strings in lines 10-12.
+    cipher_key and input_word can handle byte values to be space-separated if you prefer that.
+    """
     cipher_key = ftr.formatted("0f8866b8a9583c8dfb93848e56193d3a117372af1a967846211b87878d5305a7")
     input_word = ftr.formatted("6c8609a5dff9a0e8fdb368b81f41314f")
     action = "ENCRYPT" # must be "ENCRYPT" or"DECRYPT"
